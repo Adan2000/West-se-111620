f718e6245f1de0c8dd4af8c18a726ed02af9b3c7
adam-larosa <adam.larosa@flatironschool.com>
2020-11-23 15:29:59 -0800

    funstuff

--- 06-ruby-small-group-review/adam/lib/ManagerEmployeeContract.rb
+++ 06-ruby-small-group-review/adam/lib/ManagerEmployeeContract.rb
@@ -1,2 +1,15 @@
 class ManagerEmployeeContract
+    attr_reader :start_date, :manager, :employee
+    @@all = []
+    def initialize(date, manager, employee)
+        @start_date = date
+        @manager = manager
+        @employee = employee
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
+    
 end 
