cd3d9e4f31d403367480e3a4e2186c18696a5cec
ixnp <enprocopios@gmail.com>
2021-01-11 15:39:27 -0800

    intro to dj

---
+++ 26-intro-js/index.js
@@ -0,0 +1,60 @@
+//Data Types Js
+// undefined
+// 1 number 
+// 'rose' string
+// true bool
+// {} object
+// []
+// function(){}
+
+// BigInt(98437534753897)
+// 98437534753897n
+// Symbol('foo')
+
+//pass-by-value vs pass-by-reference
+
+let cat = 'rose'
+let pet = cat.toUpperCase()
+
+console.log('cat :', cat)
+console.log('pet :', pet)
+
+let arr = [1,2,3]
+let arr2 = arr
+arr2.push(4)
+console.log('arr :', arr)
+console.log('arr2 :', arr2)
+
+//Grabbing DOM elements 
+    //Read
+    let btn = document.getElementById('add_cat_btn')
+    let selectedCatInfoDiv = document.querySelector('#selected_cat_info')
+    let container = document.querySelector('.container')
+    let  containers = document.getElementsByClassName('container')
+    // console.log(containers[0])
+    // console.log(containers[1])
+    let lis = document.getElementsByTagName('li')
+    let catDiv = document.querySelector('#inner_cat_info')
+    let catImage = document.querySelector('#inner_cat_info img')
+    console.log(catImage)
+    //Create
+    let catchphraseDiv = document.createElement('div')
+    let p = document.createElement('p')
+    //add attributes
+    p.id  = 'party-cat-catchphrase'
+    p.className ='catchphrase'
+    p.textContent = 'Party Cat likes to Party'
+    // container.appendChild(p)
+    catchphraseDiv.appendChild(p)
+    console.log(catchphraseDiv)
+    // catDiv.insertBefore(catchphraseDiv, catImage)
+
+    //Update
+    btn.textContent = 'Add to Guest List'
+    btn.style.background = 'red'
+    catImage.src = 'party_cat.jpeg'
+    //Delete 
+    let ul = document.querySelector('#cat_ul')
+    ul.children[2].remove()
+
+//Pulling it all together 
---
+++ 26-intro-js/readme.md
@@ -0,0 +1,27 @@
+# Intro Js
+## SWBAT
+- Explain the role of HTML, CSS and JS in web development 
+- Implement a script tag to load javascript after the DOM loads
+- Explain Js Data Types
+- Understand pass-by-value and pass-by-reference
+- Grab elements from the DOM  
+
+[A Brief History of JavaScript](https://auth0.com/blog/a-brief-history-of-javascript/)
+
+
+## DOM(Document Object Model)
+- "The DOM is the data representation of the objects that comprise the structure and content of a document on the web"
+- Interface for HTML and XML
+- Represents a web page so we can make changes to the document
+- The DOM is represented as Nodes and objects
+
+
+### DOM selectors 
+```
+   document.getElementById('selected_cat_info')
+   document.querySelector('#selected_cat_info')
+   document.querySelector('.container')
+   document.getElementsByClassName('container')
+   document.getElementsByTagName('li')
+   document.querySelectorAll('div')
+```
